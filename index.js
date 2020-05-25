713ce839bb0ea0084e83812c442e68a3e845f5c2
Kognise <felix.mattick@gmail.com>
2020-05-25 17:14:27 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+
+if (!process.env.SECRET) {
+	console.log('You must set the SECRET environment variable!')
+	process.exit(1)
+}
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const fs = require('fs')
+const path = require('path')
+const uid = require('uid-promise')
+
+const directory = 'files/'
+if (!fs.existsSync(directory)) fs.mkdirSync(directory)
+
+const app = express()
+app.use(bodyParser.text())
+
+app.get('/', (req, res) => res.redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ'))
+
+app.post('/', async (req, res) => {
+	if (req.headers.authorization !== process.env.SECRET) {
+		return res.sendStatus(403)
+	}
+
+	const name = `${await uid(16)}.txt`
+	fs.writeFile(path.join(directory, name), req.body, () => {})
+
+	res.send(name)
+})
+
+app.use('/', express.static(directory))
+
+app.listen(4125)
